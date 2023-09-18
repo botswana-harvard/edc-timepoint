64d0ac120c6a7d62ba17d7212dc924da45ce199a
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-09-18 12:02:28 +0200

    Update settings.py

--- edc_timepoint/settings.py
+++ edc_timepoint/settings.py
@@ -17,8 +17,8 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_NAME = 'edc_timepoint'
 ETC_DIR = '/etc/'
-KEY_PATH = KEY_PATH = os.path.join(BASE_DIR, 'crypto_fields')
-# AUTO_CREATE_KEYS = True
+KEY_PATH = os.path.join(BASE_DIR, 'crypto_fields')
+AUTO_CREATE_KEYS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
