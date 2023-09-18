f039f3847e96cae2630079c60bfb9a650a116dd2
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-09-18 12:56:02 +0200

    Update settings.py

--- edc_timepoint/settings.py
+++ edc_timepoint/settings.py
@@ -17,7 +17,7 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_NAME = 'edc_timepoint'
 ETC_DIR = '/etc/'
-KEY_PATH = os.path.join(BASE_DIR, 'crypto_fields')
+# KEY_PATH = os.path.join(BASE_DIR, 'crypto_fields')
 # AUTO_CREATE_KEYS = True
 
 # Quick-start development settings - unsuitable for production
